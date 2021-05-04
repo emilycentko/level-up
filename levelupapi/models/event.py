4db152458224f422d4d84c8a44b669637a54a030
emilycentko <emily.centko@gmail.com>
2021-05-04 15:07:29 -0500

    Added attendees property to event class

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -6,3 +6,4 @@ class Event(models.Model):
     date = models.DateTimeField(auto_now = False, auto_now_add = False)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    attendees = models.ManyToManyField("Gamer", through="GamerEvent", related_name="attending")
